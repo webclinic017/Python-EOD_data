432564db42dab782782845bf4f4aadea71dd6136
Botond Hunyadi <botond.hunyadi@yahoo.com>
2022-06-08 19:55:07 +0300

    add requesthandler & historical share price

--- main.py
+++ main.py
@@ -6,11 +6,19 @@ import pandas as pd
 API_Token = os.environ.get('API_TOKEN')
 
 
-url = (
-    'https://eodhistoricaldata.com/api/eod/MCD.US?fmt=json&api_token={}'.format(API_Token))
+class RequestHandler():
+    def __init__(self, api_token):
+        self.api_token = api_token
 
-r = requests.get(url)
-data = json.loads(r.text)
-df = pd.DataFrame(data)
-df = df.set_index('date')
-print(df)
+    def get_historical_share_price(self, ticker):
+        url = 'https://eodhistoricaldata.com/api/eod/{}?fmt=json'.format(
+            ticker)
+        resp = requests.get(url=url, auth=(
+            'api_token', self.api_token))
+        df = pd.DataFrame(resp.json())
+        df = df.set_index('date')
+        return df
+
+
+price = RequestHandler(API_Token)
+print(price.get_historical_share_price(ticker='TSLA.US'))
